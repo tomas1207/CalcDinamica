cd9a5c92c5c205e1559a55b1b81a46658e87343f
Tomas1207 <tomasfernandes98@gmail.com>
2019-05-26 20:24:00 +0100

    Calculadora dinamica com dic

---
+++ Calc/Calcula.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+namespace Calc
+{
+    class Calcula
+    {
+        Dictionary<string, Func<int, int, int>> contas = new Dictionary<string, Func<int, int, int>>()
+        {
+            {"+", (x,y) => x + y },
+            {"/",(x,y) => x/y },
+            {"-",(x,y) => x-y },
+            {"*",(x,y) => x*y }
+        };
+        public int makeaccoutns(int n1,int n2,string sinal)
+        {
+            return contas[sinal](n1, n2);
+        }
+    }
+}
--- Calc/Form1.cs
+++ Calc/Form1.cs
@@ -1,20 +1,33 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Calc
 {
     public partial class Form1 : Form
     {
+        int n1, n2;
+        string sinalMain;
         public Form1()
         {
             InitializeComponent();
+        }
+        private void button6_Click(object sender, EventArgs e)
+        {
+            var btn = sender as Button;
+            label1.Text += btn.Text;
+        }
+        private void button10_Click(object sender, EventArgs e)
+        {
+            n2 = int.Parse(label1.Text);
+            Calcula calcula = new Calcula();
+            label1.Text = calcula.makeaccoutns(n1, n2, sinalMain).ToString();
+        }
+        private void button11_Click(object sender, EventArgs e)
+        {
+            var sinal = sender as Button;
+            sinalMain = sinal.Text;
+            n1 = int.Parse(label1.Text);
+            label1.Text = "";
         }
     }
 }
